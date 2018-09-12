76c8d081708749011dbe141a3455168e6cff4157
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-09-12 12:28:53 -0300

    incompleto

---
+++ Trabajo Practivo n1/ej12.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Ejercicio 12</title>
+    <link type="text/css" rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+        <h1>Ejercicio 12
+        <pre>
+        <?php
+        $array['Capitales']['America']['Argentina']="Buenos Aires";
+        $array['Capitales']['America']['Mexico']="México D.F.";
+        $array['Capitales']['America']['Estados Unidos']="Washington D.C.";
+        $array['Capitales']['Europa']['Francia']="París";
+        $array['Capitales']['Europa']['España']="Madrid";
+        $array['Capitales']['Europa']['Grecia']="Atenas";
+        $array['Capitales']['Asia']['Rusia']="Moscú";
+        $array['Capitales']['Asia']['Japón']="Tokio";
+        $array['Capitales']['Asia']['Filipinas']="Manila";
+        echo "Arreglo Multidimensional<br>";
+        print_r($array);
+        ?>
+        </pre>
+        </h1>
+        <br><br>
+        <a  href=index.php >Menu Trabajos Practicos <- </a>
+</body>
+</html>
