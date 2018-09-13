280469117f739ca96c7d342516eb308e54fdbc21
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-09-12 21:54:57 -0300

    modificado

--- Trabajo Practivo n2/ej7.php
+++ Trabajo Practivo n2/ej7.php
@@ -1,8 +1,8 @@
 <?php
 echo "<form action='ej72.php' method='post'>";
-for ($a = 1; $a <= $_POST['text1']; $a++){
+for ($a = 0; $a < $_POST['text1']; $a++){
     echo "<input type='number' name='texto$a'><br>";
 }
-echo "<input type='submit' value='Enviar'><input type='reset' name='borrar' value='Borrar'></form>";
-echo "<form action='ej7.html' method='get'><input type='submit' name='volver' value='Volver'></form>";
+echo "<input type='submit' value='Enviar'><input type='reset' value='Borrar'></form>";
+echo "<form action='ej7.html' method='get'><input type='submit' value='Volver'></form>";
 ?>
--- Trabajo Practivo n2/ej72.php
+++ Trabajo Practivo n2/ej72.php
@@ -1,10 +1,24 @@
+<style type="text/css">
+    tr, td {
+        border: 2px solid black;
+        text-align: center;
+        font-size: 25px;
+    }
+</style>
 <?php
 echo "<table>";
-echo "<tr>";
-foreach ($_POST as $value){
-    echo "<td>" . $value . "</td>";
+$cantidad =  count($_POST);
+$res=ceil($cantidad/2);
+for($a = 0; $a < $res; $a++) {
+    echo "<tr>";
+    for($b = 0; $b < $res; $b++) {
+        $posicion = $b + $res*$a;
+        if(isset($_POST['texto'.$posicion])){
+            echo "<td>" . $_POST['texto'.$posicion] . "</td>";
+        }
+    }
+    echo "</tr>";
 }
-echo "</tr>";
 echo "</table>";
 $media=0;
 foreach ($_POST as $value){
@@ -15,5 +29,5 @@ echo "<br>Valor minimo: " . min($_POST);
 echo "<br>La media es: " . $media/count($_POST);
 ?>
 <form action="ej7.html" method="get">
-    <input type="submit" name="volver" value="Volver">
+    <input type="submit" value="Volver">
 </form>
