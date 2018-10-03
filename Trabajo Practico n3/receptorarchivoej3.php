c2188d1b1feb2cf19649af2171b3c03aca8ffae6
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-10-03 15:25:30 -0300

    modificado

--- Trabajo Practico n3/receptorarchivoej3.php
+++ Trabajo Practico n3/receptorarchivoej3.php
@@ -1,15 +1,26 @@
 <?php
+function mostrar(){
+    $archivo = fopen($_FILES['archivo']['tmp_name'], "r");
+    while ($linea = fgets($archivo)) {
+        echo $linea . "<br>";
+    }
+    fclose($archivo);
+}
 echo "<h1>Ejercicio 3</h1>";
 if (empty($_FILES['archivo']['name'])) {
     echo "<h3>(!) No ingreso ningun archivo</h3>";
 }else {
     if (file_exists('importados')) {
         echo "<h3>Archivo copiado al directorio: importados</h3>";
-        copy($_FILES['archivo']['tmp_name'], 'importados/' . $_FILES['archivo']['name']);
+            copy($_FILES['archivo']['tmp_name'], 'importados/' . $_FILES['archivo']['name']);
+        echo "<h3>Contenido del Archivo:</h3><br>";
+        mostrar();
     } else {
         mkdir('importados');
         echo "<h3>Archivo copiado al directorio: importados</h3>";
-        copy($_FILES['archivo']['tmp_name'], 'importados/' . $_FILES['archivo']['name']);
+            copy($_FILES['archivo']['tmp_name'], 'importados/' . $_FILES['archivo']['name']);
+        echo "<h3>Contenido del Archivo:</h3><br>";
+        mostrar();
     }
 }
 ?>
