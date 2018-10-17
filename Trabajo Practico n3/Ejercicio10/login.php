fd248d74842920cf35ad5f3a1343cb104453b29e
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-10-17 17:00:42 -0300

    completo

---
+++ Trabajo Practico n3/Ejercicio10/login.php
@@ -0,0 +1,31 @@
+<?php
+function archivo(){
+    $log = date('Y/m/d') . ";" . date('H:i:s') . ";" . basename($_SERVER['PHP_SELF']);
+    $archivo = fopen('Datos\registro.log','w');
+    fwrite($archivo,$log.PHP_EOL);
+    fclose($archivo);
+}
+if (!empty($_GET['logout'])){
+    session_start();
+    session_destroy();
+    header('location: ejercicio10.html');
+}else{
+    if ($_POST['user']=='Daniel' && $_POST['pass']=='1234'){
+        if (file_exists('Datos')) {
+            archivo();
+        } else {
+            mkdir('Datos');
+            archivo();
+        }
+        session_start();
+        $_SESSION['nombre']=$_POST['user'];
+        $_SESSION['validado']=1;
+        header('location: home.php');
+    }else{
+        echo "<h2>Usuario y/o Contrasena no validos</h2>
+          <form action='ejercicio10.html' method='get'>
+            <input type='submit' value='Volver''>
+          </form>";
+    }
+}
+?>
