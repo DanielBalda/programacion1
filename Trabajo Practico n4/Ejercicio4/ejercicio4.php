3f6227f6a3cb4042ec3d9dd0bc0798bc084fad18
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-10-17 21:43:00 -0300

    terminado

---
+++ Trabajo Practico n4/Ejercicio4/ejercicio4.php
@@ -0,0 +1,22 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
+$base = "programacion1";
+$conexion = new PDO("mysql:host=$servidor;dbname=$base",$usuario,$clave);
+$sql = 'SELECT * FROM persona';
+$ejecucionSQLPDO = $conexion->prepare($sql);
+$ejecucionSQLPDO->execute();
+echo "<table border='1'>";
+while ($fila = $ejecucionSQLPDO->fetch(PDO::FETCH_ASSOC)){
+    echo "<tr>";
+    foreach ($fila as $valor){
+        echo "<td>$valor</td>";
+    }
+    echo "<td><a href='eliminar.php?id=".$fila['id']."'>Eliminar</a></td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<br><a href='agregar.html'>Agregar</a>";
+die();
+?>
---
+++ Trabajo Practico n4/Ejercicio4/eliminar.php
@@ -0,0 +1,11 @@
+<?php
+$servidor="localhost";
+$usuario="root";
+$clave="";
+$base="programacion1";
+$conexion = new PDO("mysql:host=$servidor;dbname=$base",$usuario,$clave);
+$sql="DELETE FROM persona WHERE id=".$_GET['id'];
+$ejecucionSQLPDO = $conexion->prepare($sql);
+$ejecucionSQLPDO->execute();
+header('location: ejercicio4.php');
+?>
