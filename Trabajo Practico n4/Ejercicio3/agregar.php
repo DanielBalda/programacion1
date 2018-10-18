429c65460b138c4749634683a7a01be3e77b46d0
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-10-17 21:01:13 -0300

    terminado

---
+++ Trabajo Practico n4/Ejercicio3/agregar.php
@@ -0,0 +1,13 @@
+<?php
+$servidor="localhost";
+$usuario="root";
+$clave="";
+$base="programacion1";
+$conexion = new PDO("mysql:host=$servidor;dbname=$base",$usuario,$clave);
+
+$array=array('nombre' => $_POST['nombre'],'apellido' => $_POST['apellido'],'documento'=> $_POST['documento'],'edad'=> $_POST['edad']);
+$sql="INSERT INTO persona (nombre,apellido,documento,edad) VALUE (:nombre,:apellido,:documento,:edad)";
+$ejecucionSQLPDO = $conexion->prepare($sql);
+$ejecucionSQLPDO ->execute($array);
+header('location:ejercicio3.php');
+?>
